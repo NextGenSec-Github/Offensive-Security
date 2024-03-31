c192ff8e2edc0ededf7569bbccfd1fa196ef728f
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-03-31 21:53:49 +0500

    Update ffuf_cheatsheet.sh

--- Tools/FFuF/ffuf_cheatsheet.sh
+++ Tools/FFuF/ffuf_cheatsheet.sh
@@ -1,1 +1,2 @@
-
+# Fuzz usernames in a POST request to a signup page, checking for a response indicating that the username already exists.
+ffuf -w /path/to/your/username_list -X POST -d "username=FUZZ&email=x&password=x&cpassword=x" -H "Content-Type: application/x-www-form-urlencoded" -u http://target-ip/signup -mr "username already exists"
