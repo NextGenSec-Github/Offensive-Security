eec15acb3b6e7f100f271bdde1087283132f7059
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-07 01:12:30 +0500

    Create linux_directories.sh

---
+++ Command Cheatsheets/linux_directories.sh
@@ -0,0 +1,2 @@
+# /dev/shm directory is a special-purpose directory in Linux and Unix-like operating systems. It stands for "shared memory," and it is typically used for creating and managing temporary files and shared memory segments.
+cd /dev/shm
