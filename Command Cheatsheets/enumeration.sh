c906cd58313df4cfd629f9d741b9db313d34581f
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-09 20:25:49 +0500

    Update and rename enum.sh to enumeration.sh

--- Command Cheatsheets/enumeration.sh
+++ Command Cheatsheets/enumeration.sh
@@ -13,7 +13,9 @@ enum4linux <TARGET_IP>
 # This command will enumerate usernames on the target system.
 enum4linux -U <TARGET_IP>
 
-# Hydra Command for Brute Forcing
+# =======================
+# Hydra for Brute Forcing
+# =======================
 
 # SMB Brute Force
 hydra -l <USERNAME> -P <PASSWORD_FILE> smb://<TARGET_IP>
