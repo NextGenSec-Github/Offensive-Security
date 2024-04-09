d9fe022ec41f015273514d0178ccad4283ffab19
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-09 20:26:05 +0500

    Delete Command Cheatsheets/gobuster.sh

--- Command Cheatsheets/gobuster.sh
+++
@@ -1,7 +0,0 @@
-# GOBUSTER COMMANDS
-
-gobuster dir --url <IP> -w /usr/share/wordlists/dirb/big.txt -b 404 -k -f -x php
-
-gobuster dir --url <IP> --wordlist=/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt
-
-gobuster dir --url <IP>:7125 --wordlist=/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x txt,php,html -t 30 -k | tee gobuster_7125.raw.md
