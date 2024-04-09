f38d93b8bf686dc681b2719eb70f54d76d12695e
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-09 20:26:41 +0500

    Update enumeration.sh

--- Command Cheatsheets/enumeration.sh
+++ Command Cheatsheets/enumeration.sh
@@ -23,5 +23,13 @@ hydra -l <USERNAME> -P <PASSWORD_FILE> smb://<TARGET_IP>
 # SSH Brute Force
 hydra -l <USERNAME> -P <PASSWORD_FILE> ssh://<TARGET_IP>
 
+# ==================================
+# GoBuster for Directory Enumeration
+# ==================================
 
+gobuster dir --url <IP> -w /usr/share/wordlists/dirb/big.txt -b 404 -k -f -x php
+
+gobuster dir --url <IP> --wordlist=/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt
+
+gobuster dir --url <IP>:7125 --wordlist=/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt -x txt,php,html -t 30 -k | tee gobuster_7125.raw.md
 
