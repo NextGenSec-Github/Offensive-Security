dcbcdb5c020c4dcd54052e8608ee5725b40a5b03
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-20 02:40:14 +0500

    Update info_gathering_commands.sh

--- Reconnaissance/Information Gathering/info_gathering_commands.sh
+++ Reconnaissance/Information Gathering/info_gathering_commands.sh
@@ -1,12 +1,16 @@
 # SUBDOMAIN ENUMERATION
 
-# AMASS
 # Amass is a versatile subdomain enumeration tool that can discover subdomains through passive reconnaissance techniques, DNS brute-forcing, and other methods.
 amass enum [-active] [-ip] -d google.com
 amass enum -d google.com | grep google.com # To just list subdomains
 amass enum -passive -d example.com # Passive enumeration
 amass enum -active -d example.com # Active enumeration
 amass enum -brute -w wordlist.txt -d example.com # Brute-Force enumeration
+
+# CORS Brute-Force
+ffuf -w subdomains-top1million-5000.txt -u http://10.10.10.208 -H 'Origin: http://FUZZ.crossfit.htb' -mr "Access-Control-Allow-Origin" -ignore-body
+
+
 
 
 
