7f7167123b3b78b8f2c1611dd13326e777e91ac4
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-04-25 07:29:32 +0500

    Update macOS.sh

--- Command Cheatsheets/macOS.sh
+++ Command Cheatsheets/macOS.sh
@@ -22,3 +22,67 @@ system_profiler SPSoftwareDataType # System info
 system_profiler SPPrinterDataType # Printer
 system_profiler SPApplicationsDataType # Installed apps
 system_profiler SPFrameworksDataType # Installed frameworks
+system_profiler SPDeveloperToolsDataType # Developer tools info
+system_profiler SPStartupItemDataType # Startup Items
+system_profiler SPNetworkDataType # Network Capabilities
+system_profiler SPFirewallDataType # Firewall Status
+system_profiler SPNetworkLocationDataType # Known Network
+system_profiler SPBluetoothDataType # Bluetooth Info
+system_profiler SPEthernetDataType # Ethernet Info
+system_profiler SPUSBDataType # USB info
+system_profiler SPAirPortDataType # Airport Info
+
+# Searches
+mdfind password # Show all the files that contains the word password
+mfind -name password # List all the files containing the word password in the name
+
+#Open any app
+open -a <Application Name> --hide # Open app hidden
+open some.doc -a TextEdit # Open a file in one application
+
+#Computer doesn't go to sleep
+caffeinate &
+
+#Screenshot
+# This will ask for permission to the user
+screencapture -x /tmp/ss.jpg #Save screenshot in that file
+
+#Get clipboard info
+pbpaste
+
+#system_profiler
+system_profiler --help #This command without arguments take lot of memory and time.
+system_profiler -listDataTypes
+system_profiler SPSoftwareDataType SPNetworkDataType
+
+#Network
+arp -i en0 -l -a #Print the macOS device's ARP table
+lsof -i -P -n | grep LISTEN
+smbutil statshares -a #View smb shares mounted to the hard drive
+
+#networksetup - set or view network options: Proxies, FW options and more
+networksetup -listallnetworkservices #List network services
+networksetup -listallhardwareports #Hardware ports
+networksetup -getinfo Wi-Fi #Wi-Fi info
+networksetup -getautoproxyurl Wi-Fi #Get proxy URL for Wifi
+networksetup -getwebproxy Wi-Fi #Wifi Web proxy
+networksetup -getftpproxy Wi-Fi #Wifi ftp proxy
+
+#Brew
+brew list #List installed
+brew search <text> #Search package
+brew info <formula>
+brew install <formula>
+brew uninstall <formula>
+brew cleanup #Remove older versions of installed formulae.
+brew cleanup <formula> #Remove older versions of specified formula.
+
+
+
+
+
+
+
+
+
+
