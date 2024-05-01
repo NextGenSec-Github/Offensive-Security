364baa8d7dc823b2777cbb3bb29ea2319c08f732
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-05-01 17:09:48 +0500

    Update cryptography.sh

--- Command Cheatsheets/cryptography.sh
+++ Command Cheatsheets/cryptography.sh
@@ -8,13 +8,15 @@ openssl rsa -in example.key -text -noout # Print textual representation of RSA k
 
 
 
+# ======
+# Hashes
+# ======
+shasum -a 512 <filename> # Computes the SHA-512 hash value for the file specified
+shasum -a 1 <filename> # Computes the SHA-1 hash value of the file
+shasum -a 256 <filename> # Computes the SHA-256 hash value of the file
+shasum -a 384 <filename> # Computes the SHA-384 hash value of the file
+shasum -c <hash_file> # Verify a Hash Value
+shasum -a <algorithm> <file1> <file2> # Multiple Files Hashing
+shasum -a <algorithm> -b <filename> # Output Only the Hash Value
 
-
-
-
-
-
-
-
-
-
+(Get-FileHa3p4sh -Algorithm SHA512 ’<filename>’).hash | clip # Powershell: This command generates and copies the hash directly to your clipboard.
