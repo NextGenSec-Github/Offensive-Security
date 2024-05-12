6bd13b4e9a5cacb8953a1145aed76313e7cec8fb
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-05-12 09:35:05 +0500

    Create favicon_hash.py

---
+++ Scripts/Python/favicon_hash.py
@@ -0,0 +1,11 @@
+import mmh3
+import requests
+import codecs
+
+def fav_hash(url):
+    response = requests.get(url)
+    favicon = codecs.encode(response.content,"base64")
+    fhash = mmh3.hash(favicon)
+    print(f"{url} : {fhash}")
+    return fhash
+
