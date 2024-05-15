80b549df801f05c3b131a55d4f148966921d41da
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-05-15 20:14:09 +0500

    Update cryptography.sh

--- Command Cheatsheets/cryptography.sh
+++ Command Cheatsheets/cryptography.sh
@@ -6,7 +6,13 @@ openssl rsa -in example.key -pubout # Print public key or modulus only
 openssl rsa -in example.key -noout -modulus # Print public key or modulus only
 openssl rsa -in example.key -text -noout # Print textual representation of RSA key
 
-
+# DH
+openssl dhparam -out dhparams.pem 2048 # Generate DH Parameters
+openssl genpkey -paramfile dhparams.pem -out myprivate_key.pem # Generate Private Key
+openssl pkey -in myprivate_key.pem -pubout -out mypublic_key.pem # Generate Public Key
+openssl pkeyutl -derive -inkey myprivate_key.pem -peerkey friendspublic_key.pem -out shared_secret.bin # Derive the Shared Secret Key
+openssl enc -aes-256-cbc -pass file:shared_secret.bin -in recipe.txt -out encrypted_recipe.enc # Encryption Using the Shared Secret Key
+openssl aes-256-cbc -d -in encrypted_recipe.enc -pass file:shared_secret.bin -out recipe.txt # Decryption Using the Shared Secret Key
 
 # ======
 # Hashes
