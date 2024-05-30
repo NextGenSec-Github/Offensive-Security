14d0b30d53471233343fabb93c2cae0cfac22bd7
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-05-30 13:32:09 +0500

    Create kernel.sh

---
+++ Command Cheatsheets/kernel.sh
@@ -0,0 +1,13 @@
+# Kernel Commands
+
+sudo insmod kern.ko # Insert a kernel module into the Linux kernel with insmod (Insert Module)
+sudo lsmod # Check whether the module has loaded
+dmesg # Display the kernel ring buffer messages
+sudo rmmod file # Removing the kernel module 
+sudo dmesg -c # Clear the kernel buffer 
+
+
+
+
+
+
