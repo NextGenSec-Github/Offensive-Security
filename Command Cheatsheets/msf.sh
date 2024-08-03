b9fbbd87dc113410b99d69c37066f50e01cc3387
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-08-03 11:39:25 +0500

    Update msf.sh

--- Command Cheatsheets/msf.sh
+++ Command Cheatsheets/msf.sh
@@ -48,6 +48,7 @@ msfvenom --list payloads | grep meterpreter # List all meterpreter payloads
 # ===========
 
 # Core Commands
+
 background # Backgrounds the current session
 exit # Terminate the Meterpreter session
 guid # Get the session GUID (Globally Unique Identifier)
@@ -60,6 +61,7 @@ run # Executes a Meterpreter script or Post module
 sessions # Quickly switch to another session
 
 # File System Commands
+
 cd # Will change directory
 ls # Will list files in the current directory (dir will also work)
 pwd # Prints the current working directory
@@ -78,7 +80,35 @@ netstat # Displays the network connections
 portfwd # Forwards a local port to a remote service
 route # Allows you to view and modify the routing table
 
+# System commands
 
+clearev # Clears the event logs
+execute # Executes a command
+getpid # Shows the current process identifier
+getuid # Shows the user that Meterpreter is running as
+kill # Terminates a process
+pkill # Terminates processes by name
+ps # Lists running processes
+reboot # Reboots the remote computer
+shell # Drops into a system command shell
+shutdown # Shuts down the remote computer
+sysinfo # Gets information about the remote system, such as OS
+
+# Other Commands
+
+idletime # Returns the number of seconds the remote user has been idle
+keyscan_dump # Dumps the keystroke buffer
+keyscan_start # Starts capturing keystrokes
+keyscan_stop # Stops capturing keystrokes
+screenshare # Allows you to watch the remote user's desktop in real time
+screenshot # Grabs a screenshot of the interactive desktop
+record_mic # Records audio from the default microphone for X seconds
+webcam_chat # Starts a video chat
+webcam_list # Lists webcams
+webcam_snap # Takes a snapshot from the specified webcam
+webcam_stream # Plays a video stream from the specified webcam
+getsystem # Attempts to elevate your privilege to that of local system
+hashdump # Dumps the contents of the SAM database
 
 
 
