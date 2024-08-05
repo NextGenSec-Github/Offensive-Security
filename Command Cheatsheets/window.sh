2b7ff41cae49d40eac8a7763e8ea1c7597c70814
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-08-05 22:01:03 +0500

    Create window.sh

---
+++ Command Cheatsheets/window.sh
@@ -0,0 +1,12 @@
+# Windows Commands
+
+
+whoami /all # See access tokens and more
+runas /user:domain\username cmd.exe # Create a new logon session with your creds
+runas /user:domain\username /netonly cmd.exe # Credentials for accessing certain services
+
+
+
+
+
+
