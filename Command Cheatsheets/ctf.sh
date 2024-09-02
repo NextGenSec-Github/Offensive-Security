ff03f9e1db855dae8a6a9dc921d6c1638daf4324
NextGenSec <142174128+NextGenSec-Github@users.noreply.github.com>
2024-09-02 06:53:21 -0400

    Update ctf.sh

--- Command Cheatsheets/ctf.sh
+++ Command Cheatsheets/ctf.sh
@@ -1,11 +1,24 @@
-# Enum
-sudo nmap -T4 --min-rate 1000 -sV -sC -p- -oN nmap_report <ip>
-❯ gobuster dir -u <http://10.10.34.182:1337/> -w /usr/share/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt
+# -----------
+# Enumeration
+# -----------
 
+# Network Enum
+sudo nmap -T4 --min-rate 1000 -sV -sC -p- -oN nmap_report <ip>
+
+# Subdomain Enum
+gobuster dns -d target.com -w /path/to/wordlist.txt
+
+
+# Server Enum
+gobuster dir -u <url> -w <wordlist>
+nikto -h <url>
+nikto -h http://target.url -useragent "Mozilla/5.0" # Change user agent
+
+
+# ------------
+# Exploitation
+# ------------
 
 # Brute Force
 hydra -l <username> -P <password_list> ssh://<target IP>
-
-
-
 python3 sqlmap.py -r /home/tryhackme/expose/expose.req — dump
